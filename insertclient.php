c77f6a4b5d9fac2a636efaa39b2cd34db86c49d6
fcboss <franciscolemoscaldas@hotmail.com>
2019-12-06 18:02:06 +0000

    insert_cli

--- insertclient.php
+++ insertclient.php
@@ -1,70 +1,70 @@
 
 
 <html>
- <body>
-<?php
-$host = "db.tecnico.ulisboa.pt";
-$user = "ist425473";
-$pass = "rfwz7043";
-$dsn = "mysql:host=$host;dbname=$user";
-try
-{
-$connection = new PDO($dsn, $user, $pass);
-}
-catch(PDOException $exception)
-{
-echo("<p>Error: ");
-echo($exception->getMessage());
-echo("</p>");
-exit();
-}
-$VAT = $_REQUEST['VAT'];
-$name = $_REQUEST['name'];
-$street = $_REQUEST['street'];
-$city = $_REQUEST['city'];
-$zip = $_REQUEST['zip'];
-$gender = $_REQUEST['gender'];
-
-$birth_date = $_REQUEST['birth_date'];
-$birth_date = strtotime($birth_date);
-$birth_date = date("Y-m-d", $birth_date);
-
-
-$sql_client = $connection->prepare("INSERT INTO client VALUES (:VAT, :name, :birth_date, :street, :city, :zip, :gender, :age)");
-
-if($sql_client == FALSE){
-	$info = $connection->errorInfo();				
-	echo("<p>Error: {$info[2]}</p>");
-	exit();
-}
-$test = $sql_client->execute(array(
-	":VAT" => $VAT,
-	":name" => $name,
-	":birth_date" => $birth_date,
-	":street" => $street,
-	":city" => $city,
-	":zip" => $zip,
-	":gender" => $gender,
-	":age" => 0));
-
-echo("<p>Row to be added: $sql_client</p>");
-
-if($test == FALSE){
-	$info = $connection->errorInfo();
-	echo("<h3>Client already in database.</h3>");	
-	echo("<p></p>");					
-	echo("<p>Error: {$info[2]}</p>");
-	exit();
-}
-else
-{
-						echo("<p>A Prescription was succesfully inserte</p>");
-}
-
- $connection = null;
-?>
-<form action = "searchclient.php">
-  <input type="submit" value="Go to search client page">
-</form>
- </body>
+	<body>
+		<?php
+			$host = "db.tecnico.ulisboa.pt";
+			$user = "ist425473";
+			$pass = "rfwz7043";
+			$dsn = "mysql:host=$host;dbname=$user";
+			try
+			{
+				$connection = new PDO($dsn, $user, $pass);
+			}
+			catch(PDOException $exception)
+			{
+				echo("<p>Error: ");
+				echo($exception->getMessage());
+				echo("</p>");
+				exit();
+			}
+			$VAT = $_REQUEST['VAT'];
+			$name = $_REQUEST['name'];
+			$street = $_REQUEST['street'];
+			$city = $_REQUEST['city'];
+			$zip = $_REQUEST['zip'];
+			$gender = $_REQUEST['gender'];
+			
+			$birth_date = $_REQUEST['birth_date'];
+			$birth_date = strtotime($birth_date);
+			$birth_date = date("Y-m-d", $birth_date);
+			
+			
+			$sql_client = $connection->prepare("INSERT INTO client VALUES (:VAT, :name, :birth_date, :street, :city, :zip, :gender, :age)");
+			
+			if($sql_client == FALSE){
+				$info = $connection->errorInfo();				
+				echo("<p>Error: {$info[2]}</p>");
+				exit();
+			}
+			$test = $sql_client->execute(array(
+			":VAT" => $VAT,
+			":name" => $name,
+			":birth_date" => $birth_date,
+			":street" => $street,
+			":city" => $city,
+			":zip" => $zip,
+			":gender" => $gender,
+			":age" => 0));
+			
+			echo("<p>Row to be added:</p> <p>{$sql_client->queryString}</p>");
+			
+			if($test == FALSE){
+				$info = $connection->errorInfo();
+				echo("<h3>Client already in database.</h3>");	
+				echo("<p></p>");					
+				echo("<p>Error: {$info[2]}</p>");
+				exit();
+			}
+			else
+			{
+				echo("<p>A Prescription was succesfully inserte</p>");
+			}
+			
+			$connection = null;
+		?>
+		<form action = "searchclient.php">
+			<input type="submit" value="Go to search client page">
+		</form>
+	</body>
 </html>
